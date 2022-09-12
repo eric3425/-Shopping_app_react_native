faf5ce23f5a349070eb823c0079888401f3049e7
“Eric” <ericma424@gmail.com>
2022-09-13 02:10:07 +0500

    Login and Signup screens

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -8,8 +8,8 @@ import {
   Text,
   useColorScheme,
   View,
-  TextInput, ToastAndroid, Alert, Animated, Image
-
+  TextInput, ToastAndroid, Alert, Animated, Image,
+  TouchableOpacity
 } from 'react-native';
 import {
   Avatar,
@@ -17,15 +17,17 @@ import {
   Caption,
   Paragraph,
   Drawer,
-  Button
+  Button,
+  useTheme
 } from 'react-native-paper';
 import auth from '@react-native-firebase/auth';
 
 import { connect } from "react-redux"
 import Icon from 'react-native-vector-icons/Ionicons';
 import { changeisuser } from "../Store/action/index"
-
-
+import * as Animatable from 'react-native-animatable';
+import FontAwesome from 'react-native-vector-icons/FontAwesome';
+import Feather from 'react-native-vector-icons/Feather';
 
 
 
@@ -79,9 +81,99 @@ const FadeInView = (props) => {
 }
 
 function GoogleSignIn(props) {
+
   const [user, setUser] = useState();
   const [initializing, setInitializing] = useState(true);
 
+  const [data, setData] = React.useState({
+    username: '',
+    password: '',
+    check_textInputChange: false,
+    secureTextEntry: true,
+    isValidUser: true,
+    isValidPassword: true,
+  });
+
+  const { colors } = useTheme();
+
+  // const { signIn } = React.useContext(AuthContext);
+
+  const textInputChange = (val) => {
+    if (val.trim().length >= 4) {
+      setData({
+        ...data,
+        username: val,
+        check_textInputChange: true,
+        isValidUser: true
+      });
+    } else {
+      setData({
+        ...data,
+        username: val,
+        check_textInputChange: false,
+        isValidUser: false
+      });
+    }
+  }
+
+  const handlePasswordChange = (val) => {
+    if (val.trim().length >= 8) {
+      setData({
+        ...data,
+        password: val,
+        isValidPassword: true
+      });
+    } else {
+      setData({
+        ...data,
+        password: val,
+        isValidPassword: false
+      });
+    }
+  }
+
+  const updateSecureTextEntry = () => {
+    setData({
+      ...data,
+      secureTextEntry: !data.secureTextEntry
+    });
+  }
+
+  const handleValidUser = (val) => {
+    if (val.trim().length >= 4) {
+      setData({
+        ...data,
+        isValidUser: true
+      });
+    } else {
+      setData({
+        ...data,
+        isValidUser: false
+      });
+    }
+  }
+
+  const loginHandle = (userName, password) => {
+
+    const foundUser = Users.filter(item => {
+      return userName == item.username && password == item.password;
+    });
+
+    if (data.username.length == 0 || data.password.length == 0) {
+      Alert.alert('Wrong Input!', 'Username or password field cannot be empty.', [
+        { text: 'Okay' }
+      ]);
+      return;
+    }
+
+    if (foundUser.length == 0) {
+      Alert.alert('Invalid User!', 'Username or password is incorrect.', [
+        { text: 'Okay' }
+      ]);
+      return;
+    }
+    signIn(foundUser);
+  }
 
   const signout = () => {
     auth()
@@ -110,7 +202,7 @@ function GoogleSignIn(props) {
 
   if (!user) {
     return (
-      < >
+      <ScrollView style={styles.container} >
         <View style={{ backgroundColor: "#1f1f14" }}>
           <View style={{ margin: 7 }}>
             <Icon name='menu-sharp'
@@ -120,93 +212,150 @@ function GoogleSignIn(props) {
             />
           </View>
         </View>
-        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center', backgroundColor: "white" }}>
-          <FadeInView >
-            {/* <Text
-              style={{
-                fontSize: 30,
-                fontWeight: 'bold',
-                color: '#ff6600',
-                textShadowColor: "rgb(106, 109, 124)",
-                textShadowOffset: { width: 5, height: 5 },
-                textShadowRadius: 15,
-                // marginTop: -300,
-                textAlign: "center"
-              }}>
-              SHOPPING APP
-            </Text> */}
+        <View style={styles.header}>
+          <Text style={styles.text_header}>Shopping App</Text>
+        </View>
 
-            <Image style={{ alignSelf: "center", width: 150, height: 120 }} source={require("../Images/Logo.png")} />
-
-            <SafeAreaView style={{
-              borderWidth: 1,
-              borderColor: "#ff6600",
-              backgroundColor: "#ff6600",
-              borderRadius: 17,
-              margin: 35
-            }}>
-              <Text style={{
-                fontSize: 30,
-                fontWeight: 'bold',
-                color: 'white',
-                textShadowColor: "#1f1f14",
-                textShadowOffset: { width: 5, height: 5 },
-                textShadowRadius: 15,
-                textAlign: "center",
-                marginTop: 25,
-                marginBottom: 25,
-              }}>
-                SIGN IN
-              </Text>
-
-              <TextInput
-                style={{
-                  height: 45,
-                  margin: 20,
-                  borderWidth: 1,
-                  padding: 10,
-                  backgroundColor: "#1f1f14",
-                  borderColor: "#1f1f14",
-                  borderRadius: 17,
-                }}
-                // onChangeText={onChangeNumber}
-                // value={number}
-                placeholder="Username/Email"
-              // keyboardType="numeric"
-              />
-
-              <TextInput
-              
-                style={{
-                  height: 45,
-                  marginLeft: 20,
-                  marginRight: 20,
-                  marginBottom: 30,
-                  borderWidth: 1,
-                  padding: 10,
-                  backgroundColor: "#1f1f14",
-                  borderColor: "#1f1f14",
-                  borderRadius: 17
-                }}
-                // onChangeText={onChangeNumber}
-                // value={number}
-                placeholder="Password"
-              // keyboardType="numeric"
-              />
-            </SafeAreaView>
-
-            <GoogleSigninButton style={{ margin: 30, backgroundColor: "white", alignSelf: "center", height: 65 }}
-              size={GoogleSigninButton.Size.Wide}
-
-              onPress={() => onGoogleButtonPress().then(() => console.log('Signed in with Google!'))}
-            />
+        <Animatable.View
+          animation="fadeInUpBig"
+          style={
+            [styles.footer, {
+              backgroundColor: "white"
+            }]}
+        >
+          <FadeInView>
+            <Image style={{ alignSelf: "center", width: 150, height: 140 }} source={require("../Images/Logo.png")} />
           </FadeInView>
+          <Text style={[styles.text_footer, {
+            color: '#1f1f14'
+          }]}>Username</Text>
+          <View style={styles.action}>
+            <FontAwesome
+              name="user-o"
+              color="#ff6600"
+              size={20}
+            />
+            <TextInput
+              placeholder="Your Username"
+              placeholderTextColor="#666666"
+              style={[styles.textInput, {
+                color: colors.text
+              }]}
+              autoCapitalize="none"
+              onChangeText={(val) => textInputChange(val)}
+              onEndEditing={(e) => handleValidUser(e.nativeEvent.text)}
+            />
+            {data.check_textInputChange ?
+              <Animatable.View
+                animation="bounceIn"
+              >
+                <Feather
+                  name="check-circle"
+                  color="green"
+                  size={20}
+                />
+              </Animatable.View>
+              : null}
+          </View>
+          {
+            data.isValidUser ? null :
+              <Animatable.View animation="fadeInLeft" duration={500}>
+                <Text style={styles.errorMsg}>Username must be 4 characters long.</Text>
+              </Animatable.View>
+          }
 
-        </View>
-        <View>
 
-        </View>
-      </>
+          <Text style={[styles.text_footer, {
+            color: '#1f1f14',
+            marginTop: 35
+          }]}>Password</Text>
+          <View style={styles.action}>
+            <Feather
+              name="lock"
+              color="#ff6600"
+              size={20}
+            />
+            <TextInput
+              placeholder="Your Password"
+              placeholderTextColor="#666666"
+              secureTextEntry={data.secureTextEntry ? true : false}
+              style={[styles.textInput, {
+                color: colors.text
+              }]}
+              autoCapitalize="none"
+              onChangeText={(val) => handlePasswordChange(val)}
+            />
+            <TouchableOpacity
+              onPress={updateSecureTextEntry}
+            >
+              {data.secureTextEntry ?
+                <Feather
+                  name="eye-off"
+                  color="grey"
+                  size={20}
+                />
+                :
+                <Feather
+                  name="eye"
+                  color="grey"
+                  size={20}
+                />
+              }
+            </TouchableOpacity>
+          </View>
+          {
+            data.isValidPassword ? null :
+              <Animatable.View animation="fadeInLeft" duration={500}>
+                <Text style={styles.errorMsg}>Password must be 8 characters long.</Text>
+              </Animatable.View>
+          }
+
+
+          <TouchableOpacity>
+            <Text style={{ color: '#ff6600', marginTop: 15 }}>Forgot password?</Text>
+          </TouchableOpacity>
+          <View style={styles.button}>
+            <TouchableOpacity
+              style={[styles.signIn, {
+                borderColor: '#ff6600',
+                backgroundColor: '#ff6600',
+                borderWidth: 1,
+                marginTop: 15
+              }]}
+              onPress={() => { loginHandle(data.username, data.password) }}
+            >
+
+              <View style={styles.signIn}>
+                <Text style={[styles.textSign, {
+                  color: 'white'
+                }]}>Sign In</Text>
+              </View>
+            </TouchableOpacity>
+
+            <TouchableOpacity
+              onPress={() => props.navigation.navigate('Signup')}
+              style={[styles.signIn, {
+                borderColor: '#ff6600',
+                borderWidth: 1,
+                marginTop: 15
+              }]}
+            >
+              <Text style={[styles.textSign, {
+                color: '#ff6600'
+              }]}>Sign Up</Text>
+            </TouchableOpacity>
+          </View>
+          <GoogleSigninButton style={{ margin: 30, alignSelf: "center", height: 65 }}
+            size={GoogleSigninButton.Size.Wide}
+
+            onPress={() => onGoogleButtonPress().then(() => console.log('Signed in with Google!'))}
+          />
+
+        </Animatable.View >
+
+
+        {/* </FadeInView> */}
+      </ScrollView>
     );
   }
 
@@ -299,3 +448,79 @@ function mapDispatchToProps(dispatch) {
 }
 
 export default connect(mapStateToProps, mapDispatchToProps)(GoogleSignIn)
+
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#ff6600'
+  },
+  header: {
+    flex: 1,
+    justifyContent: 'center',
+    paddingHorizontal: 20,
+    paddingBottom: 50,
+    textAlign: "center",
+    alignItems: "center",
+    marginTop: 40
+  },
+  footer: {
+    flex: 3,
+    backgroundColor: '#fff',
+    borderTopLeftRadius: 30,
+    borderTopRightRadius: 30,
+    paddingHorizontal: 30,
+    paddingVertical: 15
+  },
+  text_header: {
+    color: '#fff',
+    fontWeight: 'bold',
+    fontSize: 30,
+    textShadowColor: "black",
+    textShadowOffset: { width: 3, height: 3 },
+    textShadowRadius: 10,
+  },
+  text_footer: {
+    color: '#05375a',
+    fontSize: 18
+  },
+  action: {
+    flexDirection: 'row',
+    marginTop: 10,
+    borderBottomWidth: 1,
+    borderBottomColor: '#f2f2f2',
+    paddingBottom: 5
+  },
+  actionError: {
+    flexDirection: 'row',
+    marginTop: 10,
+    borderBottomWidth: 1,
+    borderBottomColor: '#FF0000',
+    paddingBottom: 5
+  },
+  textInput: {
+    flex: 1,
+    marginTop: Platform.OS === 'ios' ? 0 : -12,
+    paddingLeft: 10,
+    color: '#05375a',
+  },
+  errorMsg: {
+    color: '#FF0000',
+    fontSize: 14,
+  },
+  button: {
+    alignItems: 'center',
+    marginTop: 50
+  },
+  signIn: {
+    width: '100%',
+    height: 50,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 10
+  },
+  textSign: {
+    fontSize: 18,
+    fontWeight: 'bold'
+  }
+});
