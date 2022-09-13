08999ac977611e6fe55c39b3c9222ee3572308fa
“Eric” <ericma424@gmail.com>
2022-09-13 14:32:20 +0500

    Update categories.js

--- src/Components/Categories.js
+++ src/Components/Categories.js
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import {useEffect,useState} from "react"
+import { useEffect, useState } from "react"
 import {
     flex,
     FormRow,
@@ -9,12 +9,12 @@ import {
     View,
     Text,
     StatusBar,
-    Image,TouchableHighlight,TouchableOpacity, LayoutAnimation
+    Image, TouchableHighlight, TouchableOpacity, LayoutAnimation
 } from 'react-native';
 import Icon from 'react-native-vector-icons/Ionicons';
-import {connect } from "react-redux"
-import {changeisuser} from "../Store/action/index"
-import {changeiscategory} from "../Store/action/index"
+import { connect } from "react-redux"
+import { changeisuser } from "../Store/action/index"
+import { changeiscategory } from "../Store/action/index"
 
 
 
@@ -23,80 +23,80 @@ import {changeiscategory} from "../Store/action/index"
 
 
 function Categories(props) {
-//     const [user,setUser]=useState({})
+    //     const [user,setUser]=useState({})
 
 
-//     useEffect(()=>{
-//     if(props.userInfo!={}){
-//     setUser(props.userInfo)
-//     props.changeisuser(user)
+    //     useEffect(()=>{
+    //     if(props.userInfo!={}){
+    //     setUser(props.userInfo)
+    //     props.changeisuser(user)
 
 
-// }
-// },[]
-//     )
+    // }
+    // },[]
+    //     )
 
 
 
-    
-    const Category=(element)=>{
+
+    const Category = (element) => {
 
         // props.changeisuser(user)
         props.changeiscategory(element)
         props.navigation.navigate("Products")
-    
+
     }
-    const CategoryList=["Clothes","Accessories","Groceries","Mobiles","Shoes","Toys"]
+    const CategoryList = ["Clothes", "Accessories", "Groceries", "Mobiles", "Shoes", "Toys"]
     return (
         <>
-        <View  style={{backgroundColor:"rgb(80, 80, 90)"}}>
-        <View style={{ margin: 7 }}>
-                <Icon name='menu-sharp'
-                    size={40}
-                    color='#ff6600'
-                    onPress={() => props.navigation.openDrawer()}
-                />
-       
-            </View>
-    </View>
-        <ScrollView style={{ backgroundColor: "rgb(106, 109, 124)" }}>
-        <Text style={{
-            textAlign:"center",
-        fontSize: 35,
-        fontWeight: "bold",
-        color: '#ff6600',
-        textShadowColor: "rgb(106, 109, 124)",
-        textShadowOffset: { width: 1, height: 1 },
-        textShadowRadius: 15,
-        marginTop: 20
-    }}>CATEGORIES</Text>
-        <View  style={{ flexDirection: 'row', marginTop:20}}>
-                <TouchableOpacity  onPress={()=>Category(CategoryList[0])} style={{ flexDirection: 'row', width: 150, height: 150, margin: 15, borderColor: '#ff6600', borderWidth: 5, backgroundColor: 'white', color: '#ff6600', borderRadius:10  }}>
-                    <Image  source={require("../Images/ClothIcon.png")} style={{ width: 140, height: 140,borderRadius:5}} />
-                </TouchableOpacity>
-                <TouchableOpacity onPress={()=>Category(CategoryList[1])} style={{ flexDirection: 'row', width: 150, height: 150, margin: 15, borderColor: '#ff6600', borderWidth: 5, backgroundColor: 'white', color: '#ff6600', borderRadius:10  }}>
-                    <Image source={require("../Images/AccessoryIcon.png")} style={{ width: 140, height: 140,borderRadius:5 }} />
-                </TouchableOpacity>
-            </View>
+            <View style={{ backgroundColor: "#1f1f14" }}>
+                <View style={{ margin: 7 }}>
+                    <Icon name='menu-sharp'
+                        size={40}
+                        color='#ff6600'
+                        onPress={() => props.navigation.openDrawer()}
+                    />
 
-            <View style={{ flexDirection: 'row',}}>
-                <TouchableOpacity onPress={()=>Category(CategoryList[2])} style={{ flexDirection: 'row', width: 150, height: 150, margin: 15, borderColor: '#ff6600', borderWidth: 5, backgroundColor: 'white', color: '#ff6600', borderRadius:10  }}>
-                    <Image source={require("../Images/GroceryIcon.png")} style={{ width: 140, height: 140,borderRadius:5}} />
-                </TouchableOpacity>
-                <TouchableOpacity onPress={()=>Category(CategoryList[3])} style={{ flexDirection: 'row', width: 150, height: 150, margin: 15, borderColor: '#ff6600', borderWidth: 5, backgroundColor: 'white', color: '#ff6600', borderRadius:10  }}>
-                    <Image source={require("../Images/MobileIcon.png")} style={{ width: 140, height: 140,borderRadius:5}} />
-                </TouchableOpacity>
+                </View>
             </View>
+            <ScrollView style={{ backgroundColor: "white" }}>
+                <Text style={{
+                    textAlign: "center",
+                    fontSize: 35,
+                    fontWeight: "bold",
+                    color: '#ff6600',
+                    textShadowColor: "rgb(106, 109, 124)",
+                    textShadowOffset: { width: 1, height: 1 },
+                    textShadowRadius: 5,
+                    marginTop: 20
+                }}>CATEGORIES</Text>
+                <View style={{ flexDirection: 'row', marginTop: 20 }}>
+                    <TouchableOpacity onPress={() => Category(CategoryList[0])} style={styles.cardBox}>
+                        <Image source={require("../Images/ClothIcon.png")} style={styles.cardImg} />
+                    </TouchableOpacity>
+                    <TouchableOpacity onPress={() => Category(CategoryList[1])} style={styles.cardBox}>
+                        <Image source={require("../Images/AccessoryIcon.png")} style={styles.cardImg} />
+                    </TouchableOpacity>
+                </View>
 
-            <View style={{ flexDirection: 'row',}}>
-                <TouchableOpacity onPress={()=>Category(CategoryList[4])} style={{ flexDirection: 'row', width: 150, height: 150, margin: 15, borderColor: '#ff6600', borderWidth: 5, backgroundColor: 'white', color: '#ff6600', borderRadius:10  }}>
-                    <Image source={require("../Images/ShoesIcon.png")} style={{ width: 140, height: 140,borderRadius:5}} />
-                </TouchableOpacity>
-                <TouchableOpacity onPress={()=>Category(CategoryList[5])} style={{ flexDirection: 'row', width: 150, height: 150, margin: 15, borderColor: '#ff6600', borderWidth: 5, backgroundColor: 'white', color: '#ff6600', borderRadius:10 }}>
-                    <Image source={require("../Images/ToysIcon.png")} style={{ width: 140, height: 140,borderRadius:5}} />
-                </TouchableOpacity>
-            </View>
-        </ScrollView>
+                <View style={{ flexDirection: 'row', }}>
+                    <TouchableOpacity onPress={() => Category(CategoryList[2])} style={styles.cardBox}>
+                        <Image source={require("../Images/GroceryIcon.png")} style={styles.cardImg} />
+                    </TouchableOpacity>
+                    <TouchableOpacity onPress={() => Category(CategoryList[3])} style={styles.cardBox}>
+                        <Image source={require("../Images/MobileIcon.png")} style={styles.cardImg} />
+                    </TouchableOpacity>
+                </View>
+
+                <View style={{ flexDirection: 'row', }}>
+                    <TouchableOpacity onPress={() => Category(CategoryList[4])} style={styles.cardBox}>
+                        <Image source={require("../Images/ShoesIcon.png")} style={styles.cardImg} />
+                    </TouchableOpacity>
+                    <TouchableOpacity onPress={() => Category(CategoryList[5])} style={styles.cardBox}>
+                        <Image source={require("../Images/ToysIcon.png")} style={styles.cardImg} />
+                    </TouchableOpacity>
+                </View>
+            </ScrollView>
         </>
 
     )
@@ -105,18 +105,46 @@ function Categories(props) {
 
 function mapStateToProps(state) {
     return {
-        Category:state.Category,
-        userInfo:state.userInfo
+        Category: state.Category,
+        userInfo: state.userInfo
     }
-  }
-  
-  
-  function mapDispatchToProps(dispatch) {
+}
+
+
+function mapDispatchToProps(dispatch) {
     return {
-      changeiscategory:(Category)=>dispatch(changeiscategory(Category)),
-    // changeisuser:(userInfo)=>dispatch(changeisuser(userInfo))
+        changeiscategory: (Category) => dispatch(changeiscategory(Category)),
+        // changeisuser:(userInfo)=>dispatch(changeisuser(userInfo))
 
     }
-  }
-  
-  export default connect(mapStateToProps, mapDispatchToProps)(Categories)
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Categories)
+
+const styles = StyleSheet.create({
+    cardBox: {
+        flexDirection: 'row',
+        width: 150,
+        height: 150,
+        margin: 15,
+        borderColor: '#ff6600',
+        borderWidth: 3,
+        backgroundColor: 'white',
+        color: '#ff6600',
+        borderRadius: 10,
+        shadowColor: "#ff6600",
+        shadowOffset: {
+            width: 5,
+            height: 5,
+        },
+        shadowOpacity: 2,
+        shadowRadius: 6.27,
+
+        elevation: 10,
+    },
+    cardImg: {
+        width: 140,
+        height: 140,
+        borderRadius: 5
+    }
+})
